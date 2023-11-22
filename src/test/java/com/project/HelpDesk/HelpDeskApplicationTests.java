dc66f300b37a5cb08d81c1ce6b21ea0c4973bbb3
Luan Valente <luanbvalente@hotmail.com>
2023-11-22 00:04:23 -0300

    Projeto criado

---
+++ src/test/java/com/project/HelpDesk/HelpDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.HelpDesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
