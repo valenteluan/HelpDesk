a048f73b48974642179d8c4803a8e2d2bb672516
Luan Valente <luanbvalente@hotmail.com>
2023-11-24 15:50:14 -0300

    Perfil de teste

---
+++ src/main/java/com/project/HelpDesk/config/TestConfig.java
@@ -0,0 +1,22 @@
+package com.project.HelpDesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.project.HelpDesk.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public void instanciaDB() {
+		this.dbService.instanciaDB();
+	}
+	
+}
