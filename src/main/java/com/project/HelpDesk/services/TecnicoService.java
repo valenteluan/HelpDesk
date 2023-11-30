cf12c12ac0cf363681f0444611296fcfe40bf18a
Luan Valente <luanbvalente@hotmail.com>
2023-11-29 23:30:49 -0300

    Atualizar Tecnicos

--- src/main/java/com/project/HelpDesk/services/TecnicoService.java
+++ src/main/java/com/project/HelpDesk/services/TecnicoService.java
@@ -14,6 +14,8 @@ import com.project.HelpDesk.repositories.TecnicoRepository;
 import com.project.HelpDesk.services.exceptions.DataIntegrityViolationException;
 import com.project.HelpDesk.services.exceptions.ObjectNotFoundException;
 
+import jakarta.validation.Valid;
+
 @Service
 public class TecnicoService {
 
@@ -38,6 +40,14 @@ public class TecnicoService {
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
 	}
+	
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+		objDTO.setId(id);
+		Tecnico oldObj = findById(id);
+		validaPorCpfEEmail(objDTO);
+		oldObj = new Tecnico(objDTO);
+		return repository.save(oldObj);
+	}
 
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
@@ -49,4 +59,5 @@ public class TecnicoService {
 			throw new DataIntegrityViolationException("Email já cadastrado!");
 		}
 	}
+
 }
