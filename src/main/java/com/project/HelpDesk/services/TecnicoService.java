5e336280db8ab9030a94a5da02478d42cdad0b0d
Luan Valente <luanbvalente@hotmail.com>
2023-11-29 23:44:18 -0300

    Deletar Tecnicos

--- src/main/java/com/project/HelpDesk/services/TecnicoService.java
+++ src/main/java/com/project/HelpDesk/services/TecnicoService.java
@@ -48,6 +48,15 @@ public class TecnicoService {
 		oldObj = new Tecnico(objDTO);
 		return repository.save(oldObj);
 	}
+	
+
+	public void delete(Integer id) {
+		Tecnico obj = findById(id);
+		if (obj.getChamados().size() > 0) {
+			throw new DataIntegrityViolationException("Tecnico possui ordens de serviço e não pode ser deletado!");
+		}
+		repository.deleteById(id);
+	}
 
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
